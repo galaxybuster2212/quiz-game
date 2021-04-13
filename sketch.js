5e9d44b8d6b79717bf13152e7982d064a314f842
galaxybuster2212 <73293056+galaxybuster2212@users.noreply.github.com>
2021-04-13 11:54:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var canvas;
+var gameState,contestantCount,database,quiz,question,contestant;
+
+gameState = 0;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("blue");
+
+  
+}
+
+function display(){
+  this.title.html = "my quiz game"
+  this.title.position = 350,0
+  this.question.html = "question - What breed of dog does Queen Elizabeth II famously own"
+  this.question.position = 150,80
+  this.option1.html = "Finnish Spitz"
+  this.option1.html = 150,100
+  this.option2.html = "Pembroke Welsh Corgi"
+  this.option2.html = 150,120
+
+
+
+
+}
